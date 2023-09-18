1a7d41fc4ad7f12d2632c1413803d9a3bf4ed11d
Andrey Ulanov <andy.ulanov@gmail.com>
2023-09-18 20:15:01 +0400

    fix: fix errors in ImagesListService

--- ImageFeed/ImagesList/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -11,8 +11,8 @@ struct PhotoResult: Decodable {
 	let id: String
 	let width: Int
 	let height: Int
-	let createdAt: String
-	let description: String
+	let createdAt: String?
+	let description: String?
 	let urls: UrlsResult
 	let likedByUser: Bool
 	
@@ -34,7 +34,7 @@ struct Photo {
 	init(photoData: PhotoResult) {
 		id = photoData.id
 		size = CGSize(width: photoData.width, height: photoData.height)
-		createdAt = ISO8601DateFormatter().date(from: photoData.createdAt)
+		createdAt = photoData.createdAt != nil ? ISO8601DateFormatter().date(from: photoData.createdAt!) : nil
 		welcomeDescription = photoData.description
 		thumbImageURL = photoData.urls.thumb
 		largeImageURL = photoData.urls.full
@@ -47,20 +47,29 @@ final class ImagesListService {
 	
 	private let urlSession = URLSession.shared
 	private var task: URLSessionTask?
-	private(set) var photos: [Photo] = []
+	private(set) var photos: [Photo] = [] {
+		didSet {
+			NotificationCenter.default
+				.post(name: ImagesListService.didChangeNotification,
+					  object: self)
+		}
+	}
 	private var lastLoadedPage: Int?
 	
-	func fetchPhotosNextPage(completion: @escaping (Result<[Photo], Error>) -> Void) {
-		guard let url = URL(string: "\(UnsplashApiConstants.DefaultBaseURL.description)/photos")
+	func fetchPhotosNextPage() {
+		guard let url = URL(string: "\(UnsplashApiConstants.DefaultBaseURL.description)/photos"),
+			  let accessToken = OAuth2TokenStorage.shared.token
 		else {
-			assertionFailure("Failed to create URL")
+			assertionFailure("Failed to create URL or get token from storage")
 			return
 		}
 		
 		let nextPage = lastLoadedPage == nil ? 1 : lastLoadedPage! + 1
 		var request = URLRequest(url: url)
-		request.setValue(nextPage.description, forHTTPHeaderField: "page")
-		let task = urlSession.objectTask(for: request) { (result: Result<[PhotoResult], Error>) in
+		request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+		request.addValue(nextPage.description, forHTTPHeaderField: "page")
+		let task = urlSession.objectTask(for: request) { [weak self] (result: Result<[PhotoResult], Error>) in
+			guard let self else { return }
 			switch result {
 			case .success(let photosData):
 				var photos: [Photo] = []
@@ -69,12 +78,9 @@ final class ImagesListService {
 					photos.append(photo)
 				}
 				self.photos += photos
-				completion(.success(photos))
-				NotificationCenter.default
-					.post(name: ImagesListService.didChangeNotification,
-						  object: self)
+				self.lastLoadedPage = nextPage
 			case .failure(let error):
-				completion(.failure(error))
+				assertionFailure(error.localizedDescription)
 			}
 			self.task = nil
 		}
