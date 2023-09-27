edfa4cbe326f0e772db77d67270f1e64f6cf2b6f
Andrey Ulanov <andy.ulanov@gmail.com>
2023-09-27 23:53:21 +0400

    test: add ImagesList tests

---
+++ ImageFeedTests/ImagesListTests.swift
@@ -0,0 +1,42 @@
+//
+//  ImagesListTests.swift
+//  ImageFeedTests
+//
+//  Created by Andrey Ulanov on 27.09.2023.
+//
+
+import XCTest
+@testable import ImageFeed
+
+final class ImagesListTests: XCTestCase {
+	func testIfViewControllerCallsViewDidLoad() {
+		// Given
+		let viewController = ImagesListViewController()
+		let presenter = ImagesListPresenterSpy()
+		viewController.presenter = presenter
+		presenter.view = viewController
+		
+		// When
+		_ = viewController.view
+		
+		// Then
+		XCTAssertTrue(presenter.viewDidLoadCalled)
+	}
+}
+
+final class ImagesListPresenterSpy: ImagesListPresenterProtocol {
+	var viewDidLoadCalled = false
+	var view: ImageFeed.ImagesListViewControllerProtocol?
+	
+	func viewDidLoad() {
+		viewDidLoadCalled = true
+	}
+	
+	func getImages() {
+		
+	}
+	
+	func like(photo: ImageFeed.Photo, completion: (() -> Void)?) {
+		
+	}
+}
